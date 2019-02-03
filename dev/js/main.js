bc4b956c80879e136feb70181ee4df5ce4a3ae86
hitmax <43930558+hitmax@users.noreply.github.com>
2019-02-03 15:31:55 +0200

    first commit

---
+++ dev/js/main.js
@@ -0,0 +1,63 @@
+;(function ($) {
+    $(function () {
+
+        $('.hero-section-slider').slick({
+            arrows: false,
+            fade: true,
+            dots: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            pauseOnFocus: false,
+            pauseOnHover: false,
+            infinite: true
+        });
+
+        $('.section-convenience-slider').slick({
+            arrows: true,
+            fade: false,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            pauseOnFocus: false,
+            pauseOnHover: false,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1
+        });
+        $('.section-tour-packages-slider').slick({
+            centerMode: true,
+            centerPadding: '60px',
+            arrows: true,
+            fade: false,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            pauseOnFocus: false,
+            pauseOnHover: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 3
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 1
+                    }
+                }
+            ]
+        });
+
+    });
+})(jQuery);
