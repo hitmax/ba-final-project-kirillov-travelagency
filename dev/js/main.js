030b206d48dab5e4318eed5b6f054364d1ab1a84
hitmax <43930558+hitmax@users.noreply.github.com>
2019-02-12 20:14:46 +0200

    add sect GOOGLE MAP, fix some troubles with section feedback form and gave it negative margin

--- dev/js/main.js
+++ dev/js/main.js
@@ -116,6 +116,8 @@
         // ****************************************************************************
 
 
+        const $root = $('body,html');
+
         // ******************************** hide/show back-to-top-button **************
         let backToTopButton = $('#back-to-top-button'),
             firstSection = $('.hero-section'),              //cant make search of 2 first section
@@ -148,7 +150,7 @@
 
 
         backToTopButton.on('click', function () {
-            $('body,html').animate({
+            $root.animate({
                 scrollTop: 0
             }, 1000);
             // return false;                                          // !IMPORTANT for what?!?!
@@ -223,6 +225,164 @@
 
         // **************************************************************************
 
+        // ********************* PARALLAX EFFECT FOR PLANE AND SHADOW ***************
+
+        $('a[href^="#"]').click(function () {
+            $root.animate({
+                scrollTop: $($.attr(this, 'href')).offset().top
+            }, 500);
+
+            return false;
+        });
+        // **************************************************************************
+
 
     });
 })(jQuery);
+
+
+var map,
+    infoWindowContent = document.getElementsByClassName('info-map')[0].outerHTML;
+
+
+function initMap() {
+    var TravelAgency = {lat: 40.680191, lng: -73.902404},
+        map = new google.maps.Map(document.getElementById('g-map'), {
+            center: {lat: 40.670000, lng: -73.902404},
+            zoom: 14,
+            styles: [{
+                "featureType": "all",
+                "elementType": "labels.text.fill",
+                "stylers": [{"saturation": 36}, {"color": "#333333"}, {"lightness": 40}]
+            }, {
+                "featureType": "all",
+                "elementType": "labels.text.stroke",
+                "stylers": [{"visibility": "on"}, {"color": "#ffffff"}, {"lightness": 16}]
+            }, {
+                "featureType": "all",
+                "elementType": "labels.icon",
+                "stylers": [{"visibility": "off"}]
+            }, {
+                "featureType": "administrative",
+                "elementType": "all",
+                "stylers": [{"visibility": "off"}]
+            }, {
+                "featureType": "administrative",
+                "elementType": "geometry.fill",
+                "stylers": [{"color": "#fefefe"}, {"lightness": 20}]
+            }, {
+                "featureType": "administrative",
+                "elementType": "geometry.stroke",
+                "stylers": [{"color": "#fefefe"}, {"lightness": 17}, {"weight": 1.2}]
+            }, {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [{"lightness": 20}, {"color": "#ececec"}]
+            }, {
+                "featureType": "landscape.man_made",
+                "elementType": "all",
+                "stylers": [{"visibility": "on"}, {"color": "#f0f0ef"}]
+            }, {
+                "featureType": "landscape.man_made",
+                "elementType": "geometry.fill",
+                "stylers": [{"visibility": "on"}, {"color": "#f0f0ef"}]
+            }, {
+                "featureType": "landscape.man_made",
+                "elementType": "geometry.stroke",
+                "stylers": [{"visibility": "on"}, {"color": "#d4d4d4"}]
+            }, {
+                "featureType": "landscape.natural",
+                "elementType": "all",
+                "stylers": [{"visibility": "on"}, {"color": "#ececec"}]
+            }, {"featureType": "poi", "elementType": "all", "stylers": [{"visibility": "on"}]}, {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [{"lightness": 21}, {"visibility": "off"}]
+            }, {
+                "featureType": "poi",
+                "elementType": "geometry.fill",
+                "stylers": [{"visibility": "on"}, {"color": "#d4d4d4"}]
+            }, {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#303030"}]
+            }, {
+                "featureType": "poi",
+                "elementType": "labels.icon",
+                "stylers": [{"saturation": "-100"}]
+            }, {
+                "featureType": "poi.attraction",
+                "elementType": "all",
+                "stylers": [{"visibility": "on"}]
+            }, {
+                "featureType": "poi.business",
+                "elementType": "all",
+                "stylers": [{"visibility": "on"}]
+            }, {
+                "featureType": "poi.government",
+                "elementType": "all",
+                "stylers": [{"visibility": "on"}]
+            }, {
+                "featureType": "poi.medical",
+                "elementType": "all",
+                "stylers": [{"visibility": "on"}]
+            }, {
+                "featureType": "poi.park",
+                "elementType": "all",
+                "stylers": [{"visibility": "on"}]
+            }, {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [{"color": "#dedede"}, {"lightness": 21}]
+            }, {
+                "featureType": "poi.place_of_worship",
+                "elementType": "all",
+                "stylers": [{"visibility": "on"}]
+            }, {
+                "featureType": "poi.school",
+                "elementType": "all",
+                "stylers": [{"visibility": "on"}]
+            }, {
+                "featureType": "poi.school",
+                "elementType": "geometry.stroke",
+                "stylers": [{"lightness": "-61"}, {"gamma": "0.00"}, {"visibility": "off"}]
+            }, {
+                "featureType": "poi.sports_complex",
+                "elementType": "all",
+                "stylers": [{"visibility": "on"}]
+            }, {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [{"color": "#ffffff"}, {"lightness": 17}]
+            }, {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [{"color": "#ffffff"}, {"lightness": 29}, {"weight": 0.2}]
+            }, {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [{"color": "#ffffff"}, {"lightness": 18}]
+            }, {
+                "featureType": "road.local",
+                "elementType": "geometry",
+                "stylers": [{"color": "#ffffff"}, {"lightness": 16}]
+            }, {
+                "featureType": "transit",
+                "elementType": "geometry",
+                "stylers": [{"color": "#f2f2f2"}, {"lightness": 19}]
+            }, {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [{"color": "#dadada"}, {"lightness": 17}]
+            }]
+        }),
+
+        marker = new google.maps.Marker({
+            position: TravelAgency,
+            map: map,
+            title: 'TravelAgency'
+        });
+
+    // marker.setMap(map);
+    marker.initMap(map);
+}
