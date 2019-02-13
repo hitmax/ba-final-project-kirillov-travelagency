cd9cde4e56b5bf79f72a155191565393fa7da4eb
hitmax <43930558+hitmax@users.noreply.github.com>
2019-02-14 00:14:59 +0200

    add calculate

--- dev/js/appendant.js
+++ dev/js/appendant.js
@@ -56,23 +56,35 @@
             helpMeButton.fadeIn();
             helpMeBlock.fadeOut();
         });
-        callMeButton.on('click', function () {
-            // helpMeButton.fadeOut();
-            // helpMeBlock.fadeOut();
-        });
         // **************************************************************************
 
         // ********** RegExp for phone number/email/others and ARRAYS WITH DATA *****
-        const   regExpPhoneNumber = /^((\+380)+([0-9]){9})$/gm;
-                // regExpEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gm,
-                // regExpName = /^[a-zA-Z\s]+$/gm,
-                // regExpMessage = /^[a-zA-Z0-9 ]{5,}$/gm;
+        const regExpPhoneNumber = /^((\+380)+([0-9]){9})$/gm;
+        // regExpEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gm,
+        // regExpName = /^[a-zA-Z\s]+$/gm,
+        // regExpMessage = /^[a-zA-Z0-9 ]{5,}$/gm;
 
         // **************************************************************************
 
+        callMeButton.on('click', function (e) {
+            const $form = $('#form-help-me-block');
+            const phone = $form.find('input').val();
+            // const pattern = /^((\+380)+([0-9]){9})$/gm;
+
+            if (regExpPhoneNumber.test(phone)) {
+                $form.find('p.not-valid').hide();
+                helpMeButton.fadeOut();
+                helpMeBlock.fadeOut();
+            } else {
+                $form.find('p.not-valid').show();
+                e.stopImmediatePropagation();
+            }
+        });
+
+
         // ********************* FOR smoothly ANCHORS reaction and scrolling ********
 
-        $('a[href^="#"]').click(function () {
+        $('a.anchor').click(function () {
             $root.animate({
                 scrollTop: $($.attr(this, 'href')).offset().top
             }, 500);
@@ -96,18 +108,90 @@
             console.log(phoneNumber.val());
             if (!regExpPhoneNumber.test(phoneNumber.val())) {
                 e.find('p.not-valid').show();
-                phoneNumber.focus;
-                return false;
+                // phoneNumber.focus;
+                // return false;
             }
         }
 
         $('form#form-help-me-block').on('submit', function (e) {
-            e.preventDefault();
-            e.stopPropagation();
             checkPhoneNumber($(this));
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            return false;
         });
 
         // **************************************************************************
+
+        // ********************* Validating form help me block  ********************
+
+        // (function quantityProducts() {
+        //     var $quantityArrowMinus = $(".quantity-arrow-minus");
+        //     var $quantityArrowPlus = $(".quantity-arrow-plus");
+        //     var $quantityNum = $(".quantity-num");
+        //
+        //     $quantityArrowMinus.click(quantityMinus);
+        //     $quantityArrowPlus.click(quantityPlus);
+        //
+        //     function quantityMinus() {
+        //         if ($quantityNum.val() > 1) {
+        //             $quantityNum.val(+$quantityNum.val() - 1);
+        //         }
+        //     }
+        //
+        //     function quantityPlus() {
+        //         $quantityNum.val(+$quantityNum.val() + 1);
+        //     }
+        // })();
+        let $quantityNum;
+
+        function quantityMinus(elem) {
+            if (elem.val() > 0) {
+                elem.val(elem.val() - 1);
+            }
+        }
+
+        function quantityPlus(elem) {
+            elem.val(+elem.val() + 1);
+        }
+
+        function getHotelStarsCount() {
+            return $('input:radio:checked').val();
+        }
+
+        function calculateTotalSum(adults, kids, hotel, days) {
+            let totalSum;
+            if (hotel === 3) {
+                totalSum = (adults * 100 + kids * 50) * days;
+            } else if (hotel === 4) {
+                totalSum = (adults * 150 + kids * 70) * days;
+            } else {
+                totalSum = (adults * 200 + kids * 90) * days;
+            }
+            return totalSum;
+        }
+
+        $('.quantity-arrow-minus').on('click', function () {
+            let input = $(this).siblings('.quantity-num');
+            quantityMinus(input);
+        });
+
+        $('.quantity-arrow-plus').on('click', function () {
+            let input = $(this).siblings('.quantity-num');
+            quantityPlus(input);
+        });
+
+        $('#calculate-it').on('click', function () {
+            let adultsQuantity = +$('#adults-quantity').val(),
+                kidsQuantity = +$('#kids-quantity').val(),
+                hotelStars = +getHotelStarsCount(),
+                amountOfDays = +$('#amount-of-days').val();
+
+            $('#total-price').val(calculateTotalSum(adultsQuantity, kidsQuantity, hotelStars, amountOfDays));
+
+        })
+
+        // **************************************************************************
+
 
     });
 })(jQuery);
